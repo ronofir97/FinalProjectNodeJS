b9890542339d43b0e63a9b57ff72d5013275f494
Ron Ofir <48864890+ronofir97@users.noreply.github.com>
2022-06-24 16:50:32 +0300

    Update app.js

--- FinalProjectNodeJSRon&Almog/app.js
+++ FinalProjectNodeJSRon&Almog/app.js
@@ -11,7 +11,7 @@ const mongoose = require('mongoose');
 const app = express();
 
 // connection to db
-const uri = 'mongodb+srv://ronofir97:ron123456@cluster0.4yo6b.mongodb.net/FinaProject-Asynchronous?retryWrites=true&w=majority';
+const uri = '';
 mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
 const connection = mongoose.connection;
 connection.once('open', function () {
